43bb997a35be0a415b9db811cc52c14cd7ef53bc
AyushP19 <ayushpandaey.ayush7878@gmail.com>
2025-05-30 02:22:32 +0530

    Fix port binding for Render deployment

--- app.py
+++ app.py
@@ -137,6 +137,7 @@ def status():
     return jsonify({'status': 'ok'})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ['PORT'])  # <--- force it to crash if PORT is not set
+    app.run(host='0.0.0.0', port=port)
+
 
